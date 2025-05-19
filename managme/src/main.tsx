0af8d2ead55348de5e33ab5aa0e62018ddbba12c
Remigiusz Krupa <115359807+xjustride@users.noreply.github.com>
2025-05-19 20:10:53 +0200

    install google sign-in library

--- managme/src/main.tsx
+++ managme/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+  import { GoogleOAuthProvider } from '@react-oauth/google'
+
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
+
+if (!googleClientId) {
+  console.error('Google Client ID is not defined. Please check your .env file.')
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <GoogleOAuthProvider clientId={googleClientId || 'YOUR_GOOGLE_CLIENT_ID_FALLBACK_IF_NEEDED_OR_EMPTY_STRING'}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
 )
